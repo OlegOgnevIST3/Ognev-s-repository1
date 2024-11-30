51a12653a20a13d760926225e5e723e7489c9e74
OlegOgnevIST3 <olegmobileru@gmail.com>
2024-11-30 10:26:27 +0300

    changes2
    Registration, authorization, and session management via cookies have been implemented.

---
+++ login.php
@@ -0,0 +1,5 @@
+<form method="post" action="auth.php">
+    <input type="text" name="username" placeholder="Имя пользователя" required>
+    <input type="password" name="password" placeholder="Пароль" required>
+    <button type="submit" name="login">Войти</button>
+</form>
---
+++ register.php
@@ -0,0 +1,7 @@
+<form method="post" action="auth.php">
+    <input type="text" name="username" placeholder="Имя пользователя" required>
+    <input type="password" name="password" placeholder="Пароль" required>
+    <input type="color" name="bg_color" value="#ffffff"> Цвет фона
+    <input type="color" name="font_color" value="#000000"> Цвет шрифта
+    <button type="submit" name="register">Зарегистрироваться</button>
+</form>

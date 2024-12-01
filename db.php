d61644f594a723da41ee5b11187b091994ffaf9e
OlegOgnevIST3 <olegmobileru@gmail.com>
2024-12-01 09:55:00 +0300

    changes3

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=your_database', 'username', 'password');
+    $db = new PDO('mysql:host=localhost;dbname=database1', 'root', 'Popkorn11');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Ошибка подключения: " . $e->getMessage());
